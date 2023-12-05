540889f2cf0f5b9698bb1b30970dc033b7373713
Febrio <muhridlo111@gmail.com>
2023-12-05 11:37:56 +0700

    menghapus command

--- src/views/peminjaman/form.php
+++ src/views/peminjaman/form.php
@@ -215,21 +215,18 @@
     dropArea.addEventListener('drop', (event) => {
         event.preventDefault();
         // console.log('File is dropped in drag area');
-        file = event.dataTransfer.files[0]; // grab single file even of user selects multiple files
+        file = event.dataTransfer.files[0]; 
         // console.log(file);
         displayFile();
     });
 
     function displayFile() {
         let fileType = file.type;
-        // console.log(fileType);
         let validExtensions = ['image/jpeg', 'image/jpg', 'image/png'];
         if (validExtensions.includes(fileType)) {
-            // console.log('This is an image file');
             let fileReader = new FileReader();
             fileReader.onload = () => {
                 let fileURL = fileReader.result;
-                // console.log(fileURL);
                 let imgTag = `<img src="${fileURL}" alt="">`;
                 dropArea.innerHTML = imgTag;
             };
