972012fc99b01291da0ea15ca90661fa274bc161
Febrio <muhridlo111@gmail.com>
2023-12-17 20:38:12 +0700

    mengedit absrak controler

--- src/core/Controller.php
+++ src/core/Controller.php
@@ -1,17 +1,22 @@
 <?php
 
-class Controller
+abstract class AbstractController
 {
     public function view($view, $data = [])
     {
         require '../src/views/' . $view . '.php';
     }
 
+    abstract public function model($model);
+}
+
+class Controller extends AbstractController
+{
     public function model($model)
     {
-
         require '../src/models/' . $model . '.php';
 
         return new $model;
     }
 }
+
